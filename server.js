001cf3f83f700b53b75cee783c927169dd868bf1
Stefan Eggers <stefan.eggers@ibsolution.de>
2023-07-19 16:13:20 +0200

    experimat

--- server.js
+++ server.js
@@ -39,6 +39,12 @@ polka()
     
     res.end(ret);
   })
+  .get('/pos/show', (req,res)=>{
+    let posarr = getlastvalueWn("postracker");
+    let lastpos = posarr[length-1];
+    let html = "<html><head><meta http-equiv=\"refresh\" content=\"0; url='https://www.google.de/maps/place/49%C2%B004\'43.0%22N+9%C2%B003\'55.9%22E\'\" /></head><body></body></html>";
+    res.end(html);
+  })
   .get('/consumption',(req,res)=> {
     res.end(JSON.stringify(getlastvalueWn("consumption")));
   })
