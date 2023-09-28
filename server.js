41f1633a24ee5de1af125c8e96a275cdae91422b
Stefan Eggers <stefan.eggers@ibsolution.de>
2023-09-28 16:25:46 +0200

    changed csv

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ polka()
     getlastvalueWn("postracker").forEach((i)=>{
       ret += i.csv + "\n";
     })
-    res.writeHead(200, { 'Content-Type': "text/plain", 'Content-Disposition' : "attachment", 'filename' : "MYFILE.csv" });
+    res.writeHead(200, { 'Content-Type': "text/csv", 'Content-Disposition' : "attachment", 'filename' : "MYFILE.csv" });
     res.end(ret, 'utf-8');
     
   })
